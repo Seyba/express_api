d5079e05aecc6755485739138fdccb05511caffa
= <mmohmo@yahoo.com>
2024-05-07 18:48:57 -0400

    user route added

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/api/user", (req, res) => res.send({"name":"Moussa"}))
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
